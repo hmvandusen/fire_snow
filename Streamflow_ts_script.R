245dcc55f67021d72fc139c6c2fb3da91f695272
hmvandusen <hvandusen@nevada.unr.edu>
2022-02-25 09:23:13 -0800

    new moving averages

--- Streamflow_ts_script.R
+++ Streamflow_ts_script.R
@@ -2,6 +2,7 @@
 ###Streamflow time series analysis
 ##01-28-2022
 
+
 knitr::opts_chunk$set(echo = TRUE)
 #install.packages('dataRetrieval')
 library(dataRetrieval) # packaage for downloading USGS stream flow data
@@ -11,6 +12,8 @@ library(forecast)
 library(stlplus)
 library(fpp)
 library(dplyr)
+library(reshape2)
+
 
 # identifying parameters for data download
 siteNumber <- "13313000" # USGS gauge number
@@ -37,10 +40,10 @@ plot(johnsoncreek_all$Date, johnsoncreek_all$discharge_cfs, type='l', ylab='Disc
 abline(v=as.Date('2007-07-17'), col='red')
 
 #Plots of 5 years before and after
-plot(johnsoncreek_all[johnsoncreek_all$Date>=as.Date('2002-10-01') & 
-                        johnsoncreek_all$Date<=as.Date('2012-10-01'),]$Date, 
-     johnsoncreek_all[johnsoncreek_all$Date>=as.Date('2002-10-01') & 
-                        johnsoncreek_all$Date<=as.Date('2012-10-01'),]$discharge_cfs, type='l', 
+plot(johnsoncreek_all[johnsoncreek_all$Date>=as.Date('2002-01-01') & 
+                        johnsoncreek_all$Date<=as.Date('2012-01-01'),]$Date, 
+     johnsoncreek_all[johnsoncreek_all$Date>=as.Date('2002-01-01') & 
+                        johnsoncreek_all$Date<=as.Date('2012-01-01'),]$discharge_cfs, type='l', 
      ylab='Discharge (cfs)', xlab='Time (days)')
 abline(v=as.Date('2007-07-17'), col='red')
 
@@ -55,33 +58,15 @@ plot(johnsoncreek_10[johnsoncreek_10$Date>=as.Date('2005-01-01') &
 
 #Use the time series package ts to see seasonality and an long-term trend 
 #This is will all stream flow data 1986 to s2022
-johnsoncreek_all.ts<-ts(johnsoncreek_all$discharge_cfs, start = c(1986, 10), end = c(2022,01), frequency = 365)
+johnsoncreek_all.ts<-ts(johnsoncreek_all$discharge_cfs, start = c(1986, 01), end = c(2022,01), frequency = 365)
 plot(johnsoncreek_all.ts)
 johnson_all_add = decompose(johnsoncreek_all.ts, type = 'additive') #The moving average window is unknown
 plot(johnson_all_add) #clear seasonal trend, but not a clear long-term trend. Unknown what the moving average is
 plot(johnson_all_add$seasonal[1:365], type = 'l') #This seasonal trend starts in June and peaks in September. 
                                                   #A narrow stream flow seasonal peak 
-#This is just with the 10 year window 2002-10-01 to 2012-10-01
-johnsoncreek_10.ts<-ts(johnsoncreek_10$discharge_cfs, start = c(2002, 01), end = c(2013, 01), frequency = 365)
-plot(johnsoncreek_10.ts)
-johnson_10_add = decompose(johnsoncreek_10.ts, type = 'additive') #The moving average window is unknown
-plot(johnson_10_add) #clear seasonal trend, but not a clear long-term trend. Unknown what the moving average is
-                     #It appears that after the fire the runoff peaks are higher
-plot(johnson_10_add$seasonal[1:365], type = 'l') #This seasonal doesn't when peaks occurs 
-
-#Use the Painter et al 2017 method of calculating running average since the start of January 1st
-runningmean<-function(dataset,x,j){sum(dataset$discharge_cfs[1:x])/x}
-johnsoncreek_10$runmean<-NA
-
-for (i in 1:10) { 
-for (j in 1:365){
-  johnsoncreek_10$runmean[1+365*(2-1)]<-sum(johnsoncreek_10$discharge_cfs[(365*(2-1)):(1+365*(2-1))])/1
-}
-}
 
 
-#use USGS methond for running mean
-
+#use USGS methond for "running"rolling" mean
 #Check for missing days, if so, add NA rows:
 if(as.numeric(diff(range(johnsoncreek_10$Date))) != (nrow(johnsoncreek_10)+1)){
   fullDates <- seq(from=min(johnsoncreek_10$Date),
@@ -96,29 +81,41 @@ if(as.numeric(diff(range(johnsoncreek_10$Date))) != (nrow(johnsoncreek_10)+1)){
 }
 
 #creating 30 day average
-ma <- function(x,n=30){stats::filter(x,rep(1/n,n), sides=1)}
-johnsoncreek_10$rollmean <- as.numeric(ma(johnsoncreek_10$discharge_cfs))
+#This averages the previous 29 day and the current day (30) to get a moving average
+rollmean <- function(x,n=30){stats::filter(x,rep(1/n,n), sides=1)}
+johnsoncreek_10$rollmean <- as.numeric(rollmean(johnsoncreek_10$discharge_cfs)) 
+
+##calculating 30 day moving average from ts package. Too see a difference
+#This is just with the 10 year window 2002-10-01 to 2012-10-01
+johnsoncreek_10.ts<-ts(johnsoncreek_10$discharge_cfs, start = c(2002, 01), end = c(2013, 01), frequency = 365)
+plot(johnsoncreek_10.ts)
+johnson_10_add = decompose(johnsoncreek_10.ts, type = 'additive') #The moving average window is unknown
+plot(johnson_10_add) #clear seasonal trend, but not a clear long-term trend. Unknown what the moving average is
+#It appears that after the fire the runoff peaks are higher
+plot(johnson_10_add$seasonal[1:365], type = 'l') #This seasonal doesn't when peaks occurs 
+
+##Con. calculating 30 day moving average from ts package
+johnsoncreek_10.ts.MA<-ma(johnsoncreek_10.ts, order = 30, centre = TRUE)
+plot(johnsoncreek_10.ts.MA)
+johnsoncreek_10.ts.MA.df<-data.frame(Y=as.matrix(johnsoncreek_10.ts.MA), date=as.Date(as.yearmon(time(johnsoncreek_10.ts.MA))))
+names(johnsoncreek_10.ts.MA.df)[1]<-c('ma30')
+n<-dim(johnsoncreek_10)[1]
+johnsoncreek_10.ts.MA.df$Date<-johnsoncreek_10$Date[1:(n-3)]
+
+#this is a weird step but the ts object drops the last 3 entries, so I am just dropping it for all entries
+johnsoncreek_10.2<-full_join(johnsoncreek_10[(1:(n-3)),],johnsoncreek_10.ts.MA.df[,c("ma30","Date")])
+
+#ploting 30-day rolling mean vs 30-day moving average
+#It appears that the peaks are the same magnitude when the rolling average preceeds it by a bit
+plot(johnsoncreek_10.2$Date,johnsoncreek_10.2$rollmean, type='l', col='red', xlim=as.Date(c("2002-01-01", "2013-01-01")))
+lines(johnsoncreek_10.2$Date, johnsoncreek_10.2$ma30)
+
+
+####
+####
+##Calculating change in rolling/running/moving average 
 
 
 
-#Finding Quantiles
-#with dpylr 
-summaryQ <- johnsoncreek_10 %>%
-  group_by(doy) %>%
-  summarize(p75 = quantile(rollmean, probs = .75, na.rm = TRUE),
-            p25 = quantile(rollmean, probs = .25, na.rm = TRUE),
-            p10 = quantile(rollmean, probs = 0.1, na.rm = TRUE),
-            p05 = quantile(rollmean, probs = 0.05, na.rm = TRUE),
-            p00 = quantile(rollmean, probs = 0, na.rm = TRUE)) 
-#with base
-summaryQ2<- by(johnsoncreek_10, list(johnsoncreek_10$doy), function(x)
-    c(p75 = quantile(johnsoncreek_10$rollmean, probs = .75, na.rm = TRUE),
-      p25 = quantile(johnsoncreek_10$rollmean, probs = .25, na.rm = TRUE),
-      p10 = quantile(johnsoncreek_10$rollmean, probs = 0.1, na.rm = TRUE),
-      p05 = quantile(johnsoncreek_10$rollmean, probs = 0.05, na.rm = TRUE),
-      p00 = quantile(johnsoncreek_10$rollmean, probs = 0, na.rm = TRUE))
-  )
-summaryQ2<-do.call(rbind, summaryQ2)
 
 
-current.year <- as.numeric(strftime(Sys.Date(), format = "%Y"))
