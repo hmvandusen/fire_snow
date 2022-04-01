ff7c6d9fdb11fba264a18dce93a4c384eee2410c
hmvandusen <hvandusen@nevada.unr.edu>
2022-03-31 17:27:14 -0700

    new updates'

--- Snotel_Johnsoncreek.R
+++ Snotel_Johnsoncreek.R
@@ -5,6 +5,7 @@ library(stlplus)
 library(fpp) 
 library(dplyr) 
 library(reshape2) 
+library(data.table)
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) 
 
 Snotel_Johnsoncreek <- read_csv("https://wcc.sc.egov.usda.gov/reportGenerator/view_csv/customSingleStationReport/daily/439:ID:SNTL%7Cid=%22%22%7Cname/POR_BEGIN,POR_END/WTEQ::value,PREC::value,TMAX::value,TMIN::value,TAVG::value,PRCP::value", 
@@ -42,11 +43,10 @@ snotel_analysis<-function(snotel.df){
   snotel.df$month <-lubridate::month(snotel.df$Date) # add month col
   snotel.df$day <-lubridate::day(snotel.df$Date) # add day of month col
   snotel.df$doy <-lubridate::yday(snotel.df$Date)# add day of year col
-  snotel.df$wy<- lubridate::wy(snotel.df$Date)
+  snotel.df<-addWaterYear(snotel.df) #add water year
   
-  
-  snotel.df$year.fact<-as.factor(snotel.df$year)
-  snotel_ls<<-split(snotel.df, snotel.df$year.fact)
+  snotel.df$wyyear.fact<-as.factor(snotel.df$waterYear)
+  snotel_ls<<-split(snotel.df, snotel.df$wyyear.fact)
 }
 
 ####
@@ -104,10 +104,10 @@ snotel_allyears<-snotel_analysis(Snotel_2000_2022)
 #watershed_allyears.ls<-lapply(watershed_allyears, function(y) eachyear(y))
 
 #Creating a usable data frame
-snotel_metrics<-data.frame(time= 1:23,year=NA, peak_date=as.Date(NA), peakswe_mm=NA, time_of_melt=as.Date(NA), slope_melt=NA)
+snotel_metrics<-data.frame(time=1:23, wyyear=NA, peak_date=as.Date(NA), peakswe_mm=NA, time_of_melt=as.Date(NA), slope_melt=NA)
 
 for (i in 1:length(snotel_allyears)) {
-  snotel_metrics$year[i]<-snotel_allyears[[i]]$year[1]
+  snotel_metrics$wyyear[i]<-snotel_allyears[[i]]$waterYear[1]
   
   peak_swe<- max(snotel_allyears[[i]]$swe)
   snotel_metrics$peakswe_mm[i]<-peak_swe*25.4
@@ -131,15 +131,65 @@ for (i in 1:length(snotel_allyears)) {
                                       #snotel_ls[[i]][snotel_ls[[i]]$month > 1 & snotel_ls[[i]]$month < 4,]$temp_max))
   
   snotel_metrics$precip_max[i] <- snotel_ls[[i]][snotel_ls[[i]]$swe == peak_swe,]$precip_acc
-  snotel_metrics$swe_precip[i]<-snotel_ls[[i]]$swe/snotel_ls[[i]]$precip_acc
+  #snotel_metrics$swe_precip[i]<-snotel_ls[[i]]$swe/snotel_ls[[i]]$precip_acc
 } #moving average mean slope is still not working 
 
 
-snotel_metrics$precip_max[i] <- snotel_ls[[i]][snotel_ls[[i]]$swe == peak_swe,]$precip_acc
+#snotel_metrics$precip_max[i] <- snotel_ls[[i]][snotel_ls[[i]]$swe == peak_swe,]$precip_acc
 
 
 snotel_metrics$peak_date<-as.Date(format(snotel_metrics$peak_date, format="%m-%d"), "%m-%d")
 snotel_metrics$time_of_melt<-as.Date(format(snotel_metrics$time_of_melt, format="%m-%d"), "%m-%d")
+
+
+####
+#Dry day for loop
+####
+
+swe_diff <- list(NA) 
+max_dry <- numeric(length = 0)
+median_dry <- numeric(length = 0)
+
+for (j in 1:length(snotel_allyears)) {
+  swe_diff.v <- numeric(length = 0)
+  
+  #Snotel dry days after Oct 1 and before May 1: Snow Season
+  #This defines dry days as 0 accumulation 
+  snotel_drydays<-snotel_ls[[j]][snotel_ls[[j]]$precip_inc == 0.0 & (snotel_ls[[j]]$month < 5 | snotel_ls[[j]]$month > 10), ]
+  
+  #this splits up all the days that are consecutive to their own list object
+  drydaysls<-split(snotel_drydays$doy, cumsum(c(1, diff(snotel_drydays$doy) != 1)))
+  
+  median_dry[j]<-median(lengths(drydaysls))
+  max_dry[j] <-max(lengths(drydaysls))
+  
+  for (i in 1:length(drydaysls)){
+    if (length(drydaysls[[i]]) > 2 ){ # I would change this 2 to either median dry days for each year or max dry days 
+      
+      swe_diff.v[i] <- snotel_drydays[snotel_drydays$doy == min(drydaysls[[i]]),]$swe - 
+        snotel_drydays[ snotel_drydays$doy == max(drydaysls[[i]]),]$swe
+    }
+  }
+  
+  swe_diff[[j]]<-swe_diff.v
+  #names(swe_diff)<-2000:2022
+  
+}
+
+boxplot(swe_diff)
+
+swe_diff_mean<-as.data.frame(do.call(rbind, lapply(swe_diff, function(x) mean(na.omit(x)))))
+
+snotel_metrics$mean_swe_loss<-swe_diff_mean$V1
+snotel_metrics$median_dryd<-median_dry
+snotel_metrics$max_dryd<-max_dry
+
+#creating a factor for before fire and 6 years after fire
+
+snotel_metrics$before_after_f<- as.factor(with(snotel_metrics, ifelse( wyyear <= fire_year, 'prefire', 
+                                                                       ifelse(wyyear > fire_year & snotel_metrics$wyyear <= fire_year+6, 'postfire_6yr', 
+                                                                              'postfire'))))
+
 
 #Mean date of spring onset (mean slope is pretty much the same)
 mean(na.omit(snotel_metrics[snotel_metrics$year <= fire_year,]$peak_date))
@@ -151,20 +201,24 @@ mean(na.omit(snotel_metrics[snotel_metrics$year > fire_year & snotel_metrics$yea
 #mean(na.omit(snotel_metrics_precip[snotel_metrics_precip$year <= fire_year,]$rration_maPeak))
 #mean(na.omit(snotel_metrics_precip[snotel_metrics_precip$year > fire_year & snotel_metrics_precip$year <= fire_year+3,]$rration_maPeak))
 
-#creating a factor for before fire and 3 years after fire
 
-snotel_metrics$before_after_f<- as.factor(with(snotel_metrics, ifelse( year <= fire_year, 'prefire', 
-                                                                               ifelse(year > fire_year & snotel_metrics$year <= fire_year+3, 'postfire_3yr', 
-                                                                                      'postfire'))))
 boxplot(peak_date ~ before_after_f, data = snotel_metrics)
 boxplot(peakswe_mm ~ before_after_f, data = snotel_metrics)
 boxplot(slope_melt ~ before_after_f, data = snotel_metrics)
 boxplot(time_of_melt ~ before_after_f, data = snotel_metrics)
 
 ggplot(snotel_metrics %>% filter(!is.na(before_after_f)), 
-       aes(x=before_after_f, y=swe_precip, fill=before_after_f)) + 
+       aes(x=before_after_f, y=max_dryd, fill=before_after_f)) + 
   geom_boxplot(alpha=0.3) +
   theme(legend.position="none") +
   scale_fill_brewer(palette="Dark2") +
-  xlab( "Pre or Post Fire") + ylab('swe / precip') + ggtitle( "Snotel site") 
+  xlab( "Pre or Post Fire") + ylab('midwintermelt loss') + ggtitle( "Snotel site") 
+
+
+ 
+
+plot(snotel_metrics$year, snotel_metrics$median_dryd, type='l')
+
+
+
 
---
+++ Watershed_scripts.R
@@ -0,0 +1,111 @@
+
+
+###
+### Watershed statistics 
+###
+
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))##set working directory to current rscript file 
+library(sf)
+library(rgdal)
+library(terra)
+library(data.table)
+#load('.rdata')
+require(tidyverse)
+library(lubridate) #time series
+library(plyr) #rbind.fill: rbind with different columns and fill with NA
+library(data.table) #this helps with the order command
+library(terra)
+library(sf)
+
+
+##Load in necessary shapefiles
+
+SCFrast <- rast('/Volumes/MJ_INFEWS/Hannah Files/Fire_stream_flow/meanSCF.tif')
+watershds <- vect('/Volumes/MJ_INFEWS/Hannah Files/Fire_stream_flow/watershed_firesnow.shp')
+SCFrast<-project(SCFrast,crs(watershds))
+fires <- vect('/Volumes/MJ_INFEWS/Hannah Files/Fire_stream_flow/fire_severity2.shp')
+fires<- project(fires, watershds)
+
+#plot(SCFrast)
+#plot(watershds, add=T)
+
+##
+##Percent of snow in watershed
+##
+
+for (i in 1:length(watershds)) {
+      assign(paste0("GageID_",watershds$GAGE_ID[i]), terra::extract(SCFrast,watershds[i]))
+}
+
+watershed.df.ls<-mget(ls(pattern="GageID_")) #list of all data frames
+
+breaks<-c(0,0.25,1)
+tags<-c('no_snow', 'snow')
+watershed_prop<- data.frame(number = 1:length(watershds), GAGE_ID = NA, no_snow=NA, snow=NA)
+
+for (i in 1:length(watershds)) {
+  breaksT<-cut(watershed.df.ls[[i]]$meanSCF, breaks = breaks, include.lowest = T, right = F, labels = tags)
+  breaksT[is.na(breaksT)]<-'no_snow'
+  breaksT<-table(breaksT)
+  percentage<-round(prop.table(breaksT), digits = 2)
+  watershed_prop$no_snow[i]<-percentage[1] #no snow percentage
+  watershed_prop$snow[i]<-percentage[2]  #snow percentage
+  watershed_prop$GAGE_ID[i]<- sub("GageID_", "", names(watershed.df.ls)[i])
+  
+}
+
+full_join(watershds, watershed_prop, by = "Gage_ID")
+
+watershds$no_snow <- watershed_prop$no_snow[match(watershds$GAGE_ID, watershed_prop$GAGE_ID)]
+watershds$snow <- watershed_prop$snow[match(watershds$GAGE_ID, watershed_prop$GAGE_ID)]
+
+#writeVector(watershds,'/Volumes/MJ_INFEWS/Hannah Files/Fire_stream_flow/watershed_snow.shp')
+
+######
+######
+#Percent of fire in watershed
+
+fire_per<-data.frame(length =  1: length(watershds), GAGE_ID = NA, fire_percentage = NA)
+
+for (i in 1:length(watershds)) {
+  intersecttst<-terra::intersect(watershds[i], fires)
+  fire_percentage<-max(expanse(intersecttst))/expanse(watershds[i])
+  fire_per$fire_percentage[i]<-fire_percentage
+  fire_per$GAGE_ID[i]<-watershds$GAGE_ID[i]
+}
+fire_per$fire_percentage<-round(fire_per$fire_percentage*100, digits = 2)
+summary(fire_per$fire_percentage)
+fire_per.dt<-data.table(fire_per)
+fire_per.dt[order(fire_percentage, GAGE_ID)] #only 31 fires that take up more than 20%
+fire_per.dt[fire_per.dt$fire_percentage > 20, ] 
+
+watershds$percentageFire <- fire_per.dt$fire_percentage[match(watershds$GAGE_ID, fire_per.dt$GAGE_ID)]
+
+####
+##merging fire dataframe and watershed dataframe
+####
+
+watershds.sf<-st_as_sf(watershds) #I will drop geometry, but for mapping purpose do not drop geometry
+fires.sf<-st_as_sf(fires)
+
+watershdfire.ls<-list(NA)
+for (i in 1:length(watershds)) {
+  intersect <- st_intersection(watershds.sf[i,], fires.sf)
+  attArea <- intersect %>% 
+    mutate(area = st_area(.) %>% as.numeric())
+  largest_contrib_fire<- st_drop_geometry(attArea[attArea$area == max(attArea$area),]) #fire with the largest contributing area to the watershed
+  watershdfire.ls[[i]]<-largest_contrib_fire
+}
+
+watershdfire.df<-data.table::rbindlist(watershdfire.ls, use.names = T, fill=T)
+
+watershdfire.df
+watershdfire.df$TF<-floor(log10(watershdfire.df$GAGE_ID)) + 1 < 8
+
+#Add a zero to GAGE_ID if shorting than 8 digets
+watershdfire.df$GAGE_ID<-ifelse(floor(log10(watershdfire.df$GAGE_ID)) + 1 < 8, paste0("0", watershdfire.df$GAGE_ID), watershdfire.df$GAGE_ID)
+
+
+write.csv(watershdfire.df, '/Volumes/MJ_INFEWS/Hannah Files/Fire_stream_flow/whole_watershedfire.csv')
+
+
